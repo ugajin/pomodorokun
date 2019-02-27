db6619d57fa33dcfb97999f6f45a998d5b63c416
ugajin <t.ugajin0804@gmail.com>
2019-02-27 22:05:02 +0900

    add count down logic

--- lib/pomodoror/cli.rb
+++ lib/pomodoror/cli.rb
@@ -5,12 +5,29 @@ module Pomodoror
   class CLI < Thor
     desc "start", "pomodoro start"
     def start
-      puts 'start'
+      start_work
     end
 
-    desc "stop", "pomodoro stop"
-    def stop
-      puts 'stop'
+    def start_work sec = 5 # 60sec * 25min
+      while sec != 0 do
+        sec -= 1
+        output = "#{sec / 60} : #{sec % 60}"
+        print "\033[2Know working... #{output}\033[100D"
+        sleep 1
+      end
+
+      start_break
+    end
+
+    def start_break sec = 300 # 60sec * 5min
+      while sec != 0 do
+        sec -= 1
+        output = "#{sec / 60} : #{sec % 60}"
+        sleep 1
+        print "\033[2Kbreak time!!! #{output}\033[100D"
+      end
+
+      start_work
     end
   end
 end
